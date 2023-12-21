a82cd6493f93b95b4744d3159c88482c7357422c
Jahed <mdjahedahmed666@gmail.com>
2023-12-21 19:30:51 +0000

    Hero section is added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-lg">
+      <h2 className="text-4xl font-bold">Empower Your Team with Efficient Task Management</h2>
+      <p className="py-6">Streamline collaboration, track progress, and achieve success with our collaborative task management platform.</p>
+      <Link>
+      <button className="btn btn-primary">Get Started</button>
+      </Link>
+    </div>
+  </div>
+</div>
   )
 }
 
